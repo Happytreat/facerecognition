fe01509c4059a2e6e0505d06fbc399b24b7c4d4e
Happytreat <cowfats@gmail.com>
2019-04-29 11:48:08 +0800

    Add particle background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
+const particleOptions = {
+  "particles": {
+    "number": {
+      "value": 70,
+      "density": {
+         "enable": true,
+        }
+      },
+      "size": {
+        "value": 10,
+        "random": true
+      },
+      "move": {
+        "direction": "bottom",
+        "out_mode": "out"
+      },
+      "line_linked": {
+        "enable": false
+        }
+      },
+      "interactivity": {
+        "events": {
+           "onclick": {
+             "enable": true,
+              "mode": "remove"
+            }
+          },
+          "modes": {
+            "remove": {
+                "particles_nb": 10
+              }
+          }
+      }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particleOptions} />
       <Navigation />
       <Logo />
       <Rank />
