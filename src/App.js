a9d9338d8f538fd2d952662e9adfabe1a7d239f9
Happytreat <cowfats@gmail.com>
2019-04-29 23:33:25 +0800

    Create state and add onChange for input url and button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -41,17 +41,37 @@ const particleOptions = {
       }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particleOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particleOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
