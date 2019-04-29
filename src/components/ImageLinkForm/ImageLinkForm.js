fca35cedfd81d6ef5c4bd11e1a06e52940ba3ddc
Happytreat <cowfats@gmail.com>
2019-04-29 11:32:05 +0800

    Add Image Link Form

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
 	return (
@@ -7,9 +8,11 @@ const ImageLinkForm = () => {
 				{'This Magic Brain will detect faces in your pictures. Git it a try.'}
 			</p>
 			<div className='center'>
-				<input className='f4 pa2 w-70 center' type='tex' />
-				<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+				<div className=' form center pa4 br3 shadow-5 width'>
+					<input className='f4 pa2 w-70 center' type='tex' />
+					<button className='w-30 grw f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
 			</div>
+				</div>
 		</div>
 	);
 }
