598f96ee68262af0e41e76e187c21fbf5ec77022
melodiessim98 <melodiessim98@gmail.com>
2019-05-08 10:07:09 +0800

    Edit signin to return user instead of default Success. msg

--- facerecognition_api/server.js
+++ facerecognition_api/server.js
@@ -45,7 +45,7 @@ app.post("/signin", (req, res) => {
 		req.body.email === database.users[0].email &&
 		req.body.password === database.users[0].password
 	) {
-		res.json("Success.")
+		res.json(database.users[0])
 	} else {
 		res.status(400).json("Error logging in.")
 	}
