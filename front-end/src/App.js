4b58adacae777280caedcde9111327b63466e864
melodiessim98 <melodiessim98@gmail.com>
2019-05-08 12:24:38 +0800

    Update rank count each time picture is submitted

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -107,7 +107,22 @@ class App extends Component {
 		this.setState({ imageUrl: this.state.input })
 		app.models
 			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-			.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+			.then(response => {
+				if (response) {
+					fetch("http://localhost:3001/image", {
+						method: "put",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							id: this.state.user.id
+						})
+					})
+						.then(response => response.json())
+						.then(count => {
+							this.setState(Object.assign(this.state.user, { entries: count }))
+						})
+				}
+				this.displayFaceBox(this.calculateFaceLocation(response))
+			})
 			.catch(err => console.log(err))
 	}
 
