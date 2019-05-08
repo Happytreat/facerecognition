de33d4ca4697e83e3df6e3da3a71116ae0c7a81f
melodiessim98 <melodiessim98@gmail.com>
2019-05-08 16:05:59 +0800

    Fix signout to initialState

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -50,23 +50,25 @@ const particleOptions = {
 	}
 }
 
+const initialState = {
+	input: "",
+	imageUrl: "",
+	box: {},
+	route: "signin",
+	isSignedIn: false,
+	user: {
+		id: "",
+		name: "",
+		email: "",
+		entries: 0,
+		joined: ""
+	}
+}
+
 class App extends Component {
 	constructor() {
 		super()
-		this.state = {
-			input: "",
-			imageUrl: "",
-			box: {},
-			route: "signin",
-			isSignedIn: false,
-			user: {
-				id: "",
-				name: "",
-				email: "",
-				entries: 0,
-				joined: ""
-			}
-		}
+		this.state = initialState
 	}
 
 	loadUser = data => {
@@ -128,7 +130,7 @@ class App extends Component {
 
 	onRouteChange = route => {
 		if (route === "signout") {
-			this.setState({ isSignedIn: false })
+			this.setState(initialState)
 		} else if (route === "home") {
 			this.setState({ isSignedIn: true })
 		}
