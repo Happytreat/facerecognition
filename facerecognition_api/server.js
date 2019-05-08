3b88f1dd2d18ccaa3eec50a03542a96a29d27732
melodiessim98 <melodiessim98@gmail.com>
2019-05-08 15:55:47 +0800

    Edit signin to connect to database

--- facerecognition_api/server.js
+++ facerecognition_api/server.js
@@ -51,14 +51,25 @@ app.get("/", (req, res) => {
 })
 
 app.post("/signin", (req, res) => {
-	if (
-		req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password
-	) {
-		res.json(database.users[0])
-	} else {
-		res.status(400).json("Error logging in.")
-	}
+	const { email, password } = req.body
+	db.select("email", "hash")
+		.from("login")
+		.where("email", "=", email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(password, data[0].hash)
+
+			isValid
+				? db
+						.select("*")
+						.from("users")
+						.where("email", "=", email)
+						.then(user => {
+							res.json(user[0])
+						})
+						.catch(err => res.status(400).json("Unable to get user."))
+				: res.status(400).json("Wrong credentials.")
+		})
+		.catch(err => res.status(400).json("Wrong credentials."))
 })
 
 app.post("/register", (req, res) => {
