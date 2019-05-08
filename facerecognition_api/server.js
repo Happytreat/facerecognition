cdf587aa25ea8ed4e6a4b9f26f1281cd1132fa9b
melodiessim98 <melodiessim98@gmail.com>
2019-05-08 15:18:15 +0800

    Edit register to save password hash

--- facerecognition_api/server.js
+++ facerecognition_api/server.js
@@ -63,21 +63,31 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
 	const { email, name, password } = req.body
-	bcrypt.hash(password, null, null, function(err, hash) {
-		//console.log(hash)
-	})
-	db("users")
-		.returning("*")
-		.insert({
-			email: email,
-			name: name,
-			joined: new Date()
-		})
-		.then(user => {
-			console.log(user[0])
-			res.json(user[0])
-		})
-		.catch(err => res.status(400).json("Unable to register."))
+	const hash = bcrypt.hashSync(password)
+	db.transaction(trx => {
+		trx
+			.insert({
+				hash: hash,
+				email: email
+			})
+			.into("login")
+			.returning("email")
+			.then(loginEmail => {
+				return trx("users")
+					.returning("*")
+					.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date()
+					})
+					.then(user => {
+						console.log(user[0])
+						res.json(user[0])
+					})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+	}).catch(err => res.status(400).json("Unable to register."))
 })
 
 app.get("/profile/:id", (req, res) => {
