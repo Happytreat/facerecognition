c81a04b62de296edf95734337adf4ddaf2af34ec
melodiessim98 <melodiessim98@gmail.com>
2019-05-08 08:40:18 +0800

    Add facerecognition Server Backend

---
+++ facerecognition_api/server.js
@@ -0,0 +1,111 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const bcrypt = require("bcrypt-nodejs")
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const database = {
+	users: [
+		{
+			id: "123",
+			name: "John",
+			email: "john@gmail.com",
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: "124",
+			name: "Sally",
+			email: "sally@gmail.com",
+			entries: 0,
+			joined: new Date()
+		}
+	],
+	login: [
+		{
+			id: "987",
+			hash: "",
+			email: "john@gmail.com"
+		}
+	]
+}
+
+app.get("/", (req, res) => {
+	res.send(database.users)
+})
+
+app.post("/signin", (req, res) => {
+	if (
+		req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password
+	) {
+		res.json("Success.")
+	} else {
+		res.status(400).json("Error logging in.")
+	}
+})
+
+app.post("/register", (req, res) => {
+	const { email, name, password } = req.body
+	bcrypt.hash(password, null, null, function(err, hash) {
+		console.log(hash)
+	})
+	database.users.push({
+		id: "125",
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length - 1])
+})
+
+const findIdThenPerform = (res, id, returnValue, f) => {
+	let found = false
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true
+			f()
+			return res.json(returnValue)
+		}
+	})
+	if (!found) {
+		res.status(404).json("No such user")
+	}
+}
+
+app.get("/profile/:id", (req, res) => {
+	const { id } = req.params
+	findIdThenPerform(res, id, database.users, () => {})
+})
+
+app.put("/image", (req, res) => {
+	const { id } = req.body
+	let found = false
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true
+			user.entries++
+			return res.json(user.entries)
+		}
+	})
+	if (!found) {
+		res.status(404).json("No such user")
+	}
+})
+
+app.listen(3000, () => {
+	console.log("App is running on port 3000.")
+})
+
+/* 
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT  --> user
+
+*/
---
+++ front-end/src/App.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+
+// initialize with your api key. This will also work in your browser via http://browserify.org/
+const app = new Clarifai.App({
+ apiKey: 'f43b0998b19848339cf1346e1d837378'
+});
+
+
+const particleOptions = {
+  "particles": {
+    "number": {
+      "value": 70,
+      "density": {
+         "enable": true,
+        }
+      },
+      "size": {
+        "value": 10,
+        "random": true
+      },
+      "move": {
+        "direction": "bottom",
+        "out_mode": "out"
+      },
+      "line_linked": {
+        "enable": false
+        }
+      },
+      "interactivity": {
+        "events": {
+           "onclick": {
+             "enable": true,
+              "mode": "remove"
+            }
+          },
+          "modes": {
+            "remove": {
+                "particles_nb": 10
+              }
+          }
+      }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, box, route } = this.state;
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particleOptions} />
+        <Navigation onRouteChange = {this.onRouteChange} isSignedIn = {isSignedIn} />
+
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit} 
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+          : (
+              route === 'signin'
+              ? <Signin onRouteChange = {this.onRouteChange} />
+              : <Register onRouteChange = {this.onRouteChange} />
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
