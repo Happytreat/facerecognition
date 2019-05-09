84c5d188c6389a0ea920457b6e26fe211bcd57d0
melodiessim98 <melodiessim98@gmail.com>
2019-05-09 23:21:24 +0800

    Create redux store and logger in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import { createLogger } from "redux-logger"
+import { inputFieldChange } from "./reducers"
 import "./index.css"
 import App from "./containers/App"
 import * as serviceWorker from "./serviceWorker"
 import "tachyons"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+//const rootReducer = combineReducers({ })
+
+const logger = createLogger()
+const store = createStore(inputFieldChange, applyMiddleware(logger))
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
