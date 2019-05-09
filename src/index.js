f6f26b2606d123a5a948fab189b699172d7f1097
melodiessim98 <melodiessim98@gmail.com>
2019-05-09 23:27:51 +0800

    Edit to updateInputField reducer name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import { createStore, applyMiddleware } from "redux"
 import { createLogger } from "redux-logger"
-import { onInputFieldChange } from "./reducers"
+import { updateInputField } from "./reducers"
 import "./index.css"
 import App from "./containers/App"
 import * as serviceWorker from "./serviceWorker"
@@ -12,7 +12,7 @@ import "tachyons"
 //const rootReducer = combineReducers({ })
 
 const logger = createLogger()
-const store = createStore(onInputFieldChange, applyMiddleware(logger))
+const store = createStore(updateInputField, applyMiddleware(logger))
 
 ReactDOM.render(
 	<Provider store={store}>
